7756605674ad4d250782b887242bcc8c08ee63be
Amir Tavakolian <rezatva@gmail.com>
2024-08-28 15:23:00 +0330

    1- what is resolve & a simple example of resolve

---
+++ app/EmailService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+class EmailService
+{
+
+    public function send()
+    {
+        echo "sending email...";
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\EmailService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,30 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        
+        $emailService1 = new EmailService(); // traditional way of making object
+
+        // resolve() ==> Resolve a service from the service container.
+
+        $emailService2 = resolve(EmailService::class); // using service container to create an object
+
+        $emailService3 = resolve('App\EmailService');
+
+        // service container's responsibility is creating object (like a factory)
+        // resolve() function asks the container for an object & return it
+
+        /*
+          Ex:
+          a doctor asks his/her assistant (resolve()) to pas him a tool
+          there is a box which contains several tools (service container)
+          assistant checks the box and get the tool (object) and return it to the doctor
+        */
+
+        dd(
+            $emailService1 == $emailService2
+            &&
+            $emailService2 == $emailService3
+            &&
+            $emailService1 == $emailService3
+        );
     }
 }
