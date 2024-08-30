e0ad2cec83c0bf9aefb1b6630628e02caadb696d
Amir Tavakolian <rezatva@gmail.com>
2024-08-30 11:02:46 +0330

    5- differences between resolve() & app() - refactor previous commit's code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\ChatgptService;
 use App\EmailService;
 use App\GrammerCheckerService;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,40 +20,98 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
 
-        # Important #
-        // below code is not clean ==> it's not a good way for using.
-        // just read it, do not use it in your projects.
-        // in next commit i have said the correct way.
+        // first read "diffrents between resolve and app.txt"
 
-        // ===============================================================
+        // ============================================================
 
-        # just remember, resolve uses service container to create object and return.
+        # simple example of using bind ==> bind a string to a work
+        # string amir to return 'hi, im amir' work
+
+        # Ex: I tell my brother to pay the money of dinner ==> string: pay-dinner-price
+        # now my brother must do several works: get to the cachier, put of his wallet, give his atm card, tell his password
+        # so, we bind only one string (pay-dinner-price) to these several works.
+
+        // so, you can prepare a task in advance, and call that task wherever you want ==> use bind()
+
+        $this->app->bind('amir', function (Application $app) {
+            return 'hi, im amir';
+        });
+
+        dump(app('amir'));  // it does the work we have defined ==> return 'hi, im amir';
+        dump(resolve('amir')); // it does the work we have defined ==> return 'hi, im amir';
+
+        // so, in any place in your project, when ever you resolve amir, it returns a string 'hi, im amir'
+
+        // --------------------------------------------------------------
+
+        $this->app->bind('log-something', function () {
+            Log::info('log this text for me');
+            // send email
+            // query to database
+            // cache something
+            // :))
+            return 'logging has been done';
+        });
+
+        // you can do any work you want before or after Logging or doing payment :D 
+
+        dump(resolve('log-something'));  // it does the work we have defined in above
+        dump(app('log-something'));  // it does the work we have defined in above
+
+        // =============================================================
+
+        // now, we want to bind the 'creating object from ChatgptService' task to a string :D
+
+        // if you create object from a class in 100 places in your project using 'new' keyword,
+        // if you want to make changes in parameter of the class, you have to manually make changes
+        // in all the places you have used 'new' keyword.
+
+        // so, use service container ==> pass the responsibility of creating object to service container.
+        // no need to make changes in your codes anymore
 
         // ================================================================
 
+        // below code is the refactor version of the previous commit's code:
+        // use below code if the value of the parameters of fixed and not change
+        // its more clear than the previous commit's code
+
+        $this->app->bind(ChatgptService::class, function () {
+            return new ChatgptService('random-token-1');
+        });
+
+        $this->app->bind(GrammerCheckerService::class, function () {
+            return new GrammerCheckerService('random-token-2', $this->app->make(ChatgptService::class));
+        });
+
+        $this->app->bind(EmailService::class, function () {
+            return new EmailService('email-title', $this->app->make(GrammerCheckerService::class));
+        });
+
+        // ==========================================================================
+
+        // use below code if the parameters are not fixed and change, each time we need an instance from the service
+
+        $emailService = resolve(EmailService::class,
+            ['title' => 'a random title',
+                'grammerCheckerService' => resolve(GrammerCheckerService::class,
+                    ['token' => 'random-token-1',
+                        'chatgptService' => resolve(ChatgptService::class, ['token' => 'random-token-2'])])
+            ]);
+
+        // you can even
+
+        $emailService1 = app(EmailService::class,
+            ['title' => 'a random title',
+                'grammerCheckerService' => resolve(GrammerCheckerService::class,
+                    ['token' => 'random-token-1',
+                        'chatgptService' => resolve(ChatgptService::class, ['token' => 'random-token-2'])])
+            ]);
+
+
+        // ==========================================================================
+
         // traditional way in php pure:
-        $emailServicee = new EmailService('a simple title', new GrammerCheckerService('my-token', new ChatgptService('random-token')));
-
-        $emailService1 = resolve(EmailService::class, ['title' => 'a simple title',
-            'grammerCheckerService' => resolve(GrammerCheckerService::class, ['token' => 'my-Token',
-                'chatgptService' => resolve(ChatgptService::class, ['token' => 'random-token'])
-            ])
-        ]);
-
-        dd($emailService1);
-
-        /*
-         # remmember: laravel service container checks the paramters of the classes constructor in resolving process.
-                      if they have dependencies on other classes, it will resolve them (if we provide required paramters)
-
-           now, even if the dependencies of a class, has dependency to other classes, service container will check them
-           and resolve them if we provide required paramters.
-
-           so ==> service container, recursively, checks all the dependencies of classes
-                  and even the dependencies of the dependencies of classes and resolve them
-                  if we provide required paramters.
-
-        */
+        $emailService = new EmailService('title', new GrammerCheckerService('token', new ChatgptService('token')));
 
     }
 }
